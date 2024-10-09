fa7e8df40a8d9313ebf11cf47597ad36de199610
Lukas Alex Jochim <lukas.jochim@tum.de>
2024-10-09 18:53:53 +0200

    Fix Coding Guidelines.

--- TennisApp/Ranking Subsystem/PlayerCellView.swift
+++ TennisApp/Ranking Subsystem/PlayerCellView.swift
@@ -7,16 +7,12 @@
 
 import SwiftUI
 
-let mockRanking = "999."
-
 struct PlayerCellView: View {
     var body: some View {
         Text("Hello Player")
             .font(Font.system(size: 22, weight: .bold))
-            
         }
     }
-
 
 #Preview {
     PlayerCellView()
