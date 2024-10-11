5daa0069b24914079f4f30b9d878439d7ee1a0b9
Lukas Alex Jochim <lukas.jochim@tum.de>
2024-10-11 19:51:35 +0200

    Add Player Images. Add Image to PlayerDetailView.

--- TennisApp/Ranking Subsystem/PlayerDetailView.swift
+++ TennisApp/Ranking Subsystem/PlayerDetailView.swift
@@ -10,8 +10,15 @@ import SwiftUI
 struct PlayerDetailView: View {
     @Environment(Model.self) private var model: Model
     var id: Int
+    var image: Image {
+        if UIImage(named: String(id)) != nil {
+            Image(String(id))
+        } else {
+            Image(systemName: "person")
+        }
+    }
     var body: some View {
-        Text("Hello " + String(id))
+        image
     }
 }
 
