8528117113ef6bbab7e8f5f8bccd05973a123775
Lukas Alex Jochim <lukas.jochim@tum.de>
2024-10-15 15:00:21 +0200

    Change placement of favorite Player Filter Button. Change Button behaviour for a server error/no favorite players saved yet. Change Background color of navigation title of RankingOverview.

--- TennisApp/Ranking Subsystem/PlayerOverview.swift
+++ TennisApp/Ranking Subsystem/PlayerOverview.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct PlayerOverview: View {
     @Environment(RankingModel.self) private var model: RankingModel
+    @State var filterFavoritePlayer: Bool = false
     var body: some View {
         NavigationStack {
-            RankingView()
+            RankingView(filterFavoritePlayer: $filterFavoritePlayer)
                 .navigationTitle("Ranking")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button(action: {
+                            if !model.ranking.players.isEmpty {
+                                filterFavoritePlayer.toggle()
+                            }
+                        }) {
+                            Image(systemName: filterFavoritePlayer ? "star.fill" : "star")
+                                .foregroundColor(.orange)
+                                .imageScale(.large)
+                        }
+                    }
+                }
         }
+        .background(Color(UIColor.systemGroupedBackground))
     }
 }
 
